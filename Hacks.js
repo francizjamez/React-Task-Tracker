923b46a04eb760df55d373ae32bc9d50347f2c85
francizjamez <francizjamez@gmail.com>
2021-02-08 21:55:24 +0800

    Build

---
+++ Hacks.js
@@ -0,0 +1,27 @@
+let array = ['a', 'b', 'c'];
+
+function combine(array){
+
+}
+
+// function combine(array){
+//     let part1 = array[0];
+//     let part2 = array[1];
+//     return part1 + part2;
+// }
+
+
+console.log(combine(array));
+
+let person = {
+
+    name: 'Benz',
+    age : 12
+}
+
+function showInfo({name, age}){
+
+    console.log(name + " is " + age + " years old");
+}
+
+showInfo(person)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(true)
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: 'Running',
+      day: 'Monday',
+      reminder: true,
+    },
+    {
+      id: 1,
+      text: 'Walking',
+      day: 'Tuesday',
+      reminder: false
+    },
+  ])
+
+  //Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ?
+          { ...task, reminder: !task.reminder } :
+          task
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd ={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggleReminder={toggleReminder} />
+      ) : (
+          'No tasks to show'
+        )
+      }
     </div>
   );
 }
+
+// class App extends React.Component {
+
+//   render(){
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
